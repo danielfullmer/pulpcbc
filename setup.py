5502901eefe5afa4e1afba88701a443e0c7b958f
Daniel Fullmer <daniel.fullmer@byu.edu>
2012-09-13 01:37:21 +0000

    Use pkg-config to determine link and compile arguments.

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
+import shlex
+import subprocess
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+proc = subprocess.Popen(['pkg-config', 'cbc', '--cflags'],
+                        stdout=subprocess.PIPE)
+compile_args, err = proc.communicate()
+compile_args = shlex.split(compile_args)
+
+proc = subprocess.Popen(['pkg-config', 'cbc', '--libs'],
+                        stdout=subprocess.PIPE)
+link_args, err = proc.communicate()
+link_args = shlex.split(link_args)
+
 ext_modules = [Extension('pulpcbc',
-                         ['pulpcbc.pyx', 'CyEventHandler.cpp'],
-                         libraries=['CbcSolver', 'Cbc', 'Cgl', 'Osi', 'OsiCbc',
-                                    'OsiClp', 'Clp', 'CoinUtils', 'rt', 'bz2',
-                                    'z', 'blas', 'lapack'],
-                         extra_compile_args=['-fpermissive'],
-                         extra_link_args=['-Wl,--no-as-needed'],
+                         extra_compile_args=['-fpermissive'] + compile_args,
+                         extra_link_args=link_args,
                          language="c++",
                         )]
 
 setup(
-    name = 'PuLP-CBC',
-    version = '0.1',
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = ext_modules,
+    name='PuLP-CBC',
+    version='0.1',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules,
 )
