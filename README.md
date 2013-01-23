29cd7d3b260cdec4d5d14f89bd0fb325d8deae7b
Daniel Fullmer <daniel.fullmer@byu.edu>
2013-01-22 22:39:43 -0700

    Updated dependencies.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A more direct Cython layer between PuLP and CBC. PuLP-CBC supports callbacks.
 
 ## Installation
 
-    $ sudo apt-get install coinor-libosi-dev coinor-libcoinutils-dev coinor-libclp-dev coinor-libcbc-dev
+    $ sudo apt-get install coinor-libosi-dev coinor-libcoinutils-dev coinor-libclp-dev coinor-libcbc-dev coinor-libcgl-dev libblas-dev liblapack-dev
     $ git clone https://github.com/danielfullmer/pulpcbc.git
     $ cd pulpcbc
     $ pip install -r requirements.txt
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from Cython.Distutils import build_ext
 
 ext_modules = [Extension('pulpcbc',
                          ['pulpcbc.pyx', 'CyEventHandler.cpp'],
-                         libraries=['CbcSolver', 'Cbc', 'Cgl', 'Osi', 'OsiCbc',
-                                    'OsiClp', 'Clp', 'CoinUtils', 'rt', 'bz2',
-                                    'z', 'blas', 'lapack'],
+                         libraries=['CbcSolver', 'Cbc', 'Cgl', 'Osi', 'OsiClp',
+                                    'Clp', 'CoinUtils', 'rt', 'bz2', 'z',
+                                    'blas', 'lapack'],
                          extra_compile_args=['-fpermissive'],
                          extra_link_args=['-Wl,--no-as-needed'],
                          language="c++",
